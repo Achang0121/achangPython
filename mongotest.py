ac97817dc3bbcd5db07dfca6867bacf7314cefed
achang0121 <13901406174@163.com>
2019-05-05 19:22:40 +0800

    新文件:       mongotest.py mongodb的基础的CRUD操作

---
+++ mongotest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from pymongo import MongoClient
+
+client = MongoClient('127.0.0.1', 27017)
+db = client.louplus
+
+db.users.insert_many([
+    {'user_id': 1000, 'name': 'Shiyan', 'pass': 10, 'study_time': 50,},
+    {'user_id': 2000, 'name': 'Lou', 'pass': 15, 'study_time':171,}
+])
+
+
